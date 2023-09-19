4be9aca2a12d9acce8d43a184cda4177e41e52c0
Jihye Jeong <bananaqick@gmail.com>
2023-09-19 22:18:38 +0900

    feat : padding 컴포넌트 추가

---
+++ src/components/common/BasicPadding.tsx
@@ -0,0 +1,9 @@
+import { styled } from "styled-components";
+
+export const BasicPadding = ({ children }: { children: React.ReactNode }) => {
+  return <PaddingContainer>{children}</PaddingContainer>;
+};
+
+const PaddingContainer = styled.div`
+  padding: 0 120px;
+`;
