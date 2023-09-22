4180680552654098ca5c79fbefc840f48477b149
DDIDDI <bananaqick@gmail.com>
2023-09-22 15:14:29 +0900

    feat : 메뉴 라벨 생성 및 리스트 생성

--- src/components/common/MenuLabel.tsx
+++ src/components/common/MenuLabel.tsx
@@ -1,33 +1,37 @@
 import styled from "styled-components";
 
 interface MenuLabelPropsType {
-  children: React.ReactNode;
-  $menuColor: string;
-  $isSelected?: boolean;
-  onClick?: () => void;
+    children: React.ReactNode;
+    $menuColor: string;
+    $isSelected?: boolean;
+    onClick?: () => void;
 }
 
 export const MenuLabel = ({
-  children,
-  $menuColor,
-  $isSelected,
-  onClick,
+    children,
+    $menuColor,
+    $isSelected,
+    onClick,
 }: MenuLabelPropsType) => {
-  return (
-    <Label $menuColor={$menuColor} onClick={onClick} $isSelected={$isSelected}>
-      {children}
-    </Label>
-  );
+    return (
+        <Label
+            $menuColor={$menuColor}
+            onClick={onClick}
+            $isSelected={$isSelected}
+        >
+            {children}
+        </Label>
+    );
 };
 
 const Label = styled.span<MenuLabelPropsType>`
-  padding: 6px 12px;
-  background-color: ${(props) =>
-    props.$isSelected ? `${props.$menuColor}30` : ""};
-  color: ${(props) => props.$menuColor};
-  font-size: 14px;
-  font-weight: 600;
-  border-radius: 16px;
-  border: 2px solid ${(props) => props.$menuColor}50;
-  cursor: pointer;
+    padding: 6px 12px;
+    background-color: ${(props) =>
+        props.$isSelected ? `${props.$menuColor}30` : ""};
+    color: ${(props) => props.$menuColor};
+    font-size: 13px;
+    font-weight: 600;
+    border-radius: 16px;
+    border: 2px solid ${(props) => props.$menuColor}50;
+    cursor: pointer;
 `;
