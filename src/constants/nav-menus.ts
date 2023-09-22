d6bfe1085356ac6c9b57b11e9c64d11e85143f74
Suzy-Lee <mulddang2@naver.com>
2023-09-23 01:40:53 +0900

    feat: Nav 컴포넌트 추가
    1. react-router-dom, react-icons 추가,
    2. Nav 컴포넌트 추가

--- src/constants/nav-menus.ts
+++ src/constants/nav-menus.ts
@@ -1,6 +1,17 @@
-export const NAV_MENUS: { path: string; title: string }[] = [
+export const NAV_MENUS: {
+  path: string;
+  title: string;
+  subList?: { path: string; title: string }[];
+}[] = [
   { path: 'search', title: '밥친구구해요' },
   { path: 'neighborhood', title: '내근처모임' },
-  { path: 'information', title: '모임정보' },
+  {
+    path: 'myapply',
+    title: '모임정보',
+    subList: [
+      { path: 'myapply', title: '나의 신청내역' },
+      { path: 'check', title: '모임 수락 / 거절' },
+    ],
+  },
   { path: 'mypage', title: '마이페이지' },
 ];
