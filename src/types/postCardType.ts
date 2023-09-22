16c57f6c5ae55cbbce02efb0627d03ca74a93046
DDIDDI <bananaqick@gmail.com>
2023-09-22 13:45:23 +0900

    feat : 게시물 타입 생성

---
+++ src/types/postCardType.ts
@@ -0,0 +1,13 @@
+export interface PostCardType {
+    id: number,
+    date: string,
+    title: string,
+    meetingName: string,
+    when: string,
+    where: string,
+    maximum: number,
+    current: number,
+    imageUrl: string,
+    nickname: string,
+    participationStatus: boolean,
+}
