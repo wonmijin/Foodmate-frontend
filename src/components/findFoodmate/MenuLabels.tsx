a9ea0f0e9f31f0e23a509ed8a938cfa6783056e5
DDIDDI <bananaqick@gmail.com>
2023-09-23 14:25:45 +0900

    feat : 메뉴 라벨 색상 상수로 수정

--- src/components/findFoodmate/MenuLabels.tsx
+++ src/components/findFoodmate/MenuLabels.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import styled from "styled-components";
 import { MenuLabel } from "../common/MenuLabel";
 import { AiFillCloseCircle } from "react-icons/ai";
+import { LABELCOLOR } from "../../constants/menu";
 
 interface MenuLabelsPropsType {
     handleMenuLabelModal: (bool: boolean) => void;
@@ -9,65 +10,6 @@ interface MenuLabelsPropsType {
 
 export const MenuLabels = ({ handleMenuLabelModal }: MenuLabelsPropsType) => {
     const [selectedMenus, setSelectedMenus] = useState<string[]>([]);
-
-    const lebelsColor = [
-        {
-            menu: "족발·보쌈",
-            color: "#B9693C",
-        },
-        {
-            menu: "찜·탕·찌개",
-            color: "#3C67B9",
-        },
-        {
-            menu: "돈까스·회·일식",
-            color: "#3CB9B9",
-        },
-        {
-            menu: "피자",
-            color: "#643CB9",
-        },
-        {
-            menu: "고기·구이",
-            color: "#B99D3C",
-        },
-        {
-            menu: "양식",
-            color: "#8C1C1C",
-        },
-        {
-            menu: "치킨",
-            color: "#9DC307",
-        },
-        {
-            menu: "중식",
-            color: "#505050",
-        },
-        {
-            menu: "아시안",
-            color: "#FFA800",
-        },
-        {
-            menu: "백반·죽·국수",
-            color: "#41B9ED",
-        },
-        {
-            menu: "도시락",
-            color: "#79B93C",
-        },
-        {
-            menu: "분식",
-            color: "#FF8F8F",
-        },
-        {
-            menu: "카페·디저트",
-            color: "#902679",
-        },
-        {
-            menu: "패스트푸드",
-            color: "#E63B3B",
-        },
-    ];
 
     const handleLabels = (menu: string) => {
         const updatedSelectedMenus = selectedMenus.includes(menu)
@@ -78,7 +20,7 @@ export const MenuLabels = ({ handleMenuLabelModal }: MenuLabelsPropsType) => {
 
     return (
         <MenuLabelsContainer>
-            {lebelsColor.map((item, idx) => {
+            {LABELCOLOR.map((item, idx) => {
                 const isSelected = selectedMenus.includes(item.menu);
 
                 return (
