7043cd767f442d797a087518b6a96001da600239
wonmijin <mijin950503@gmail.com>
2023-09-23 19:08:12 +0900

    feat:이메일비밀번호입력구현
    feat:이메일비밀번호입력구현

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,41 +1,44 @@
-import React, { useRef, useState } from "react";
-import { useForm } from "react-hook-form";
-import styled from "styled-components";
+import React from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { styled } from "styled-components";
 
-interface FormValue {
-    email: string;
-    password: string;
-}
-
-const LoginForm = (props : any) => {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-        getValues,
-    } = useForm<FormValue>({ mode: "onChange "});
+type FormValues = {
+  email: string;
+  password: string;
 };
 
-return(
-    <form onSubmit={handleSubmit{props.onSubmit}}>
-        <div>
-            <label htmlFor="email">이메일</label>
-            <input
-                id="email"
-                type="text"
-                placeholder="test@email.com"
-                {...register("email", {
-                    required: "이메일은 필수 입력입니다.",
-                    pattern: {
-                    value:
-                        /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
-                    message: "이메일 형식에 맞지 않습니다.",
-                    },
-                })}
-            />
-        </div>
-        {errors.email && <small role="alert">{errors.email.message}</small>}
-    </form>
-)
+const Login: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
+  const onSubmit: SubmitHandler<FormValues> = (data) => console.log(data);
 
-export default LoginForm
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          placeholder="이메일"
+          {...register('email', { required: '이메일을 입력해주세요' })}
+        />
+        {errors.email && <p>{errors.email.message}</p>}
+        <input
+          type="password"
+          placeholder="비밀번호"
+          {...register('password', {
+            required: '비밀번호를 입력해주세요',
+            minLength: {
+              value: 6,
+              message: '최소 6자 이상의 비밀번호를 입력해주세요',
+            },
+          })}
+        />
+        {errors.password && <p>{errors.password.message}</p>}
+        <input type="submit" />
+      </form>
+    </div>
+  );
+}
+
+export default Login;
