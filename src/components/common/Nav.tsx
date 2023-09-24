6d98990ce153c0a8b694a659d40f3df4785ea142
Suzy-Lee <mulddang2@naver.com>
2023-09-24 18:15:41 +0900

    refactor: Nav padding값 수정

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -13,7 +13,6 @@ const StyledNavContainer = styled.div`
   margin: 0 auto;
   width: 100%;
   height: 50px;
-  padding: 12px auto;
   background-color: #fff;
   box-shadow: 0px 2px 6px 0px rgba(0, 0, 0, 0.05);
 `;
@@ -24,7 +23,7 @@ const NavContent = styled.div`
   width: 1248px;
   align-items: center;
   margin: 0 auto;
-  padding: 0 20px;
+  padding: var(--basic-padding);
   position: relative;
 
   h1 {
