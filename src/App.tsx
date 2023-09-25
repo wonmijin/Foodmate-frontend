8af3dd0fdcbf812cad13ed93c34fa6d40db8de4a
DDIDDI <bananaqick@gmail.com>
2023-09-25 20:58:11 +0900

    refactor : BrowserRouter App -> main

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
-import { BrowserRouter } from 'react-router-dom';
 import Nav from './components/common/Nav';
 
 export const App = (): JSX.Element => {
-  return (
-    <BrowserRouter>
-      <Nav />
-    </BrowserRouter>
-  );
+  return <Nav />;
 };
