5714ebd653eec57462cd49700854998d46e269eb
DDIDDI <bananaqick@gmail.com>
2023-09-27 08:52:28 +0900

    feature : 모임만들기 페이지 생성

---
+++ src/pages/CreateGroupPost.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const CreateGroupPost = () => {
+  return <CreateGroupPostContainer></CreateGroupPostContainer>;
+};
+
+const CreateGroupPostContainer = styled.div``;
