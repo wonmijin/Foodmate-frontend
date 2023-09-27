3d6499f494fbbde6809105f1f0079afb152abeee
DDIDDI <bananaqick@gmail.com>
2023-09-28 01:56:50 +0900

    refactor : type 수정

--- src/components/kakao/getGeocode.ts
+++ src/components/kakao/getGeocode.ts
@@ -4,7 +4,7 @@ const geocoder = new kakao.maps.services.Geocoder();
 
 export const getGeocode = (address: string) => {
   return new Promise((resolve, reject) => {
-    geocoder.addressSearch(address, (result: { x: unknown; y: unknown }[], status: unknown) => {
+    geocoder.addressSearch(address, (result: { x: string; y: string }[], status: string) => {
       if (status === kakao.maps.services.Status.OK) {
         const coords = new kakao.maps.LatLng(result[0].x, result[0].y);
         resolve(coords);
