1e3aa5d030a4b6e369065c61a23627e2cfcd845d
Suzy-Lee <mulddang2@naver.com>
2023-09-27 10:36:46 +0900

    fix: 폰트 medium 오류 수정
    - 개발자도구에서 not found로 해당 부분 수정
    - 다양한 폰트굵기 경로 추가

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -4,10 +4,33 @@ export const GlobalStyle = createGlobalStyle`
   :root {
     --basic-padding: 0 120px;
   }
+  @font-face {
+    font-family: "Spoqa Han Sans Neo";
+    font-weight: 100;
+    src: url('../assets/fonts/SpoqaHanSansNeo-Thin.otf') format("otf");
+  }
 
   @font-face {
-    font-family: "spoqa";
-    src: url('../assets/fonts/SpoqaHanSansNeo-Medium.otf');
+    font-family: "Spoqa Han Sans Neo";
+    font-weight: 300;
+    src: url('../assets/fonts/SpoqaHanSansNeo-Light.otf') format("otf");
+  }
+  @font-face {
+    font-family: "Spoqa Han Sans Neo";
+    font-weight: 400;
+    src: url('../assets/fonts/SpoqaHanSansNeo-Regular.otf') format("otf");
+  }
+
+  @font-face {
+    font-family: "Spoqa Han Sans Neo";
+    font-weight: 500;
+    src: url('../assets/fonts/SpoqaHanSansNeo-Medium.otf') format("otf");
+  }
+
+  @font-face {
+    font-family: "Spoqa Han Sans Neo";
+    font-weight: 700;
+    src: url('../assets/fonts/SpoqaHanSansNeo-Bold.otf') format("woff2");
   }
 
   *, *::before, *::after {
