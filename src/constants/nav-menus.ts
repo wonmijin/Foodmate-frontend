8e1fc666621204278bc4ef9c1bb172e86d415b59
DDIDDI <bananaqick@gmail.com>
2023-10-06 21:17:29 +0900

    refactor : 신청 내역, 요청 조회 path 수정

--- src/constants/nav-menus.ts
+++ src/constants/nav-menus.ts
@@ -9,8 +9,8 @@ export const NAV_MENUS: {
     path: 'meeting-info',
     title: '모임 정보',
     subList: [
-      { path: 'history', title: '신청 내역' },
-      { path: 'inquiry', title: '요청 조회' },
+      { path: 'meeting-info/history', title: '신청 내역' },
+      { path: 'meeting-info/inquiry', title: '요청 조회' },
     ],
   },
   { path: 'mypage', title: '마이페이지' },
