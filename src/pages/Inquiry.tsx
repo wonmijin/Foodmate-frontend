4965943c96fee9cf6e151f487d52917ab8eb7aac
DDIDDI <bananaqick@gmail.com>
2023-10-07 00:10:24 +0900

    feat : 요청 조회 페이지 생성

--- src/pages/Inquiry.tsx
+++ src/pages/Inquiry.tsx
@@ -1,16 +1,48 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import { BasicPadding } from '../components/common/BasicPadding';
 import { SideMenu } from '../components/common/SideMenu';
 import { MEETING_INFO_MENU } from '../constants/menu';
+import { MeetingInfoRequestCardList } from '../components/meetingInfo/MeetingInfoRequestCardList';
 
 export const Inquiry = () => {
+  const [selectedCategory, setSelectedCategory] = useState('받은 요청');
+
+  const handleCategory = (category: string) => {
+    setSelectedCategory(category);
+  };
+
   return (
     <BasicPadding>
       <InquiryContainer>
         <h2>모임 정보 - 요청 조회</h2>
         <div className="contents-box">
-          <SideMenu sideMenuList={MEETING_INFO_MENU} />
-          <Contents>요청 조회 페이지 내용</Contents>
+          <SideMenu sideMenuList={MEETING_INFO_MENU} navMenuIdx={2} />
+          <Contents>
+            <div className="category">
+              <div className="category-list">
+                <div
+                  onClick={() => handleCategory('받은 요청')}
+                  className={selectedCategory === '받은 요청' ? 'selected-category' : ''}
+                >
+                  받은 요청
+                </div>
+                <div
+                  onClick={() => handleCategory('처리된 요청')}
+                  className={selectedCategory === '처리된 요청' ? 'selected-category' : ''}
+                >
+                  처리된 요청
+                </div>
+              </div>
+            </div>
+
+            <hr />
+
+            <p>3개월 이내의 신청 내역까지 확인할 수 있어요.</p>
+
+            {/* inquiry에서 분기처리 후 데이터를 해당 컴포넌트에 내려줘야 함 */}
+            <MeetingInfoRequestCardList />
+          </Contents>
         </div>
       </InquiryContainer>
     </BasicPadding>
