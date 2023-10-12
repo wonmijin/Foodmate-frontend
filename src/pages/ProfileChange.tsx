d5e865cdca491423e52eba18cda9bd0ec2ce6c82
wonmijin <mijin950503@gmail.com>
2023-10-12 16:19:47 +0900

    style: style추가
    style: style추가

--- src/pages/ProfileChange.tsx
+++ src/pages/ProfileChange.tsx
@@ -9,7 +9,11 @@ export const ProfileChange = () => {
       <BasicPadding>
         <ProfileWrap>
           <SideMenu sideMenuList={MYPAGE_CATEGORY} navMenuIdx={3} />
-          <div>프로필 수정</div>
+          <ProfileBox>
+            <h3>프로필 수정</h3>
+            <hr></hr>
+            <hr></hr>
+          </ProfileBox>
         </ProfileWrap>
       </BasicPadding>
     </>
@@ -19,4 +23,21 @@ export const ProfileChange = () => {
 const ProfileWrap = styled.div`
   display: flex;
   margin: 120px 0;
+`;
+
+const ProfileBox = styled.div`
+  width: 640px;
+  margin: 0 auto;
+
+  h3 {
+    font-size: 24px;
+    color: #212121;
+    text-align: center;
+    margin-bottom: 50px;
+  }
+
+  hr {
+    width: 640px;
+    border: 1px solid #f1f1f1;
+  }
 `;
