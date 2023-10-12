0c2a2d165bb174376931e5bc0ee2fc7b953faeae
Suzy-Lee <mulddang2@naver.com>
2023-10-12 23:11:56 +0900

    feat: input 입력시, 전송 버튼 활성화

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -1,6 +1,7 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 import { CiClock2 } from 'react-icons/ci';
 import { MdArrowForwardIos } from 'react-icons/md';
+import React, { useState } from 'react';
 
 interface MessageProps {
   chatRoomId: number | null;
@@ -24,6 +25,11 @@ const ChatMeetingTitle = styled.div`
   height: 30px;
   color: #464444;
 
+  .back-button {
+    transform: rotate(180deg);
+    cursor: pointer;
+  }
+
   .chat-meeting-title-contents {
     margin-left: 15px;
 
@@ -45,8 +51,8 @@ const ChatMessageProfile = styled.div`
   margin-right: 10px;
 
   .Chat-message-profile {
-    width: 40px;
-    height: 40px;
+    width: 30px;
+    height: 30px;
     border: 1px solid #ffce00;
     border-radius: 50%;
 
@@ -74,16 +80,13 @@ const ChatMessageMain = styled.main`
   padding: 20px 24px;
 `;
 
-const ChatMessageMainContents = styled.div`
+const ChatSenderContents = styled.div`
   display: flex;
   align-items: center;
 
   img {
     width: 32px;
     height: 32px;
-  }
-
-  .text-message-container {
   }
 
   .text-message {
@@ -102,13 +105,59 @@ const ChatMessageMainContents = styled.div`
   }
 `;
 
+const ChatRecipientContents = styled.div`
+  display: flex;
+  justify-content: flex-end;
+
+  .text-message {
+    background-color: ${(props) => props.theme.color.YELLOW};
+    padding: 18px 16px;
+    border-radius: 10px;
+    font-size: 15px;
+  }
+
+  .text-message-time {
+    text-align: right;
+    margin-top: 5px;
+    color: #737376;
+    font-size: 13px;
+  }
+`;
+
+const ChatMessageInputContainer = styled.div`
+  width: 100%;
+  padding: 20px 0 20px 24px;
+  border-top: 1px solid #ebebeb;
+  position: absolute;
+  bottom: 0;
+
+  input {
+    border: none;
+    width: 80%;
+
+    &:focus {
+      outline: none;
+    }
+  }
+`;
+
+const SendButton = styled.button<{ disabled: boolean }>`
+  background-color: ${(props) => (props.disabled ? '#F2F2F2' : '#FFCE00')};
+  border-radius: 8px;
+  padding: 8px 12px;
+`;
 
 const Message = ({ chatRoomId, closeMessage }: MessageProps) => {
+  const [inputMessage, setInputMessage] = useState<string>('');
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputMessage(e.target.value);
+  };
+
   return (
     <>
       <ChatMessageHeader>
         <ChatMeetingTitle>
-          <MdArrowForwardIos onClick={() => closeMessage()} style={{ transform: 'rotate(180deg)' }} />
+          <MdArrowForwardIos className="back-button" onClick={() => closeMessage()} />
           <div className="chat-meeting-title-contents">
             <span>푸드메이트 (${chatRoomId})</span>
             <div className="chat-list-time">
@@ -130,7 +179,7 @@ const Message = ({ chatRoomId, closeMessage }: MessageProps) => {
         </ChatPersonInfo>
       </ChatMessageHeader>
       <ChatMessageMain>
-        <ChatMessageMainContents>
+        <ChatSenderContents>
           <img src="/src/assets/chat-foodmate-default-profile.svg" alt="푸드메이트 채팅 프로필 사진" />
           <div className="text-message-container">
             <div className="text-message">
@@ -139,8 +188,18 @@ const Message = ({ chatRoomId, closeMessage }: MessageProps) => {
             </div>
             <div className="text-message-time">오후 1:00</div>
           </div>
-        </ChatMessageMainContents>
+        </ChatSenderContents>
+        <ChatRecipientContents>
+          <div className="text-message-container">
+            <div className="text-message">오늘 참여 가능한가요?</div>
+            <div className="text-message-time">오후 1:03</div>
+          </div>
+        </ChatRecipientContents>
       </ChatMessageMain>
+      <ChatMessageInputContainer>
+        <input autoFocus type="text" value={inputMessage} onChange={onChange} />
+        <SendButton disabled={inputMessage.length !== 0 ? false : true}>전송</SendButton>
+      </ChatMessageInputContainer>
     </>
   );
 };
