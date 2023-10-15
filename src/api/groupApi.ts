ec0af29423d9e3d49a308cc47519a6bc01977bd7
Suzy-Lee <mulddang2@naver.com>
2023-10-16 01:03:12 +0900

    feat: 오늘모임 컴포넌트의 API 연동

--- src/api/groupApi.ts
+++ src/api/groupApi.ts
@@ -1,8 +1,14 @@
 import axios from 'axios';
 import QuickSearchType from '../types/quickSearchType';
 import Pagination from '../types/pagination';
+import TodayMeetingType from '../types/todayMeetingType';
 
 export const quickSearchByKeyword = async (keyword: string): Promise<Pagination<QuickSearchType>> => {
   const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/group/search?keyword=${keyword}`);
+  return data;
+};
+
+export const todayMeeting = async (page: number): Promise<Pagination<TodayMeetingType>> => {
+  const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/group/today?page=${page}`);
   return data;
 };
