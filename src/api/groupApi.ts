74295d190a7612a809f2f0b6cb0e26dd1f4b9631
Suzy-Lee <mulddang2@naver.com>
2023-10-16 10:12:00 +0900

    refactor: API path 수정

--- src/api/groupApi.ts
+++ src/api/groupApi.ts
@@ -4,11 +4,11 @@ import Pagination from '../types/pagination';
 import TodayMeetingType from '../types/todayMeetingType';
 
 export const quickSearchByKeyword = async (keyword: string): Promise<Pagination<QuickSearchType>> => {
-  const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/group/search?keyword=${keyword}`);
+  const { data } = await axios.get(`/api/group/search?keyword=${keyword}`);
   return data;
 };
 
 export const todayMeeting = async (page: number): Promise<Pagination<TodayMeetingType>> => {
-  const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/group/today?page=${page}`);
+  const { data } = await axios.get(`/api/group/today?page=${page}`);
   return data;
 };
--- src/api/rankingApi.ts
+++ src/api/rankingApi.ts
@@ -2,20 +2,19 @@ import axios from 'axios';
 import { LikesRankingType, MeetingRankingType } from '../types/ranking';
 
 export const getLikesRanking = async (): Promise<LikesRankingType[]> => {
-  const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/ranking/likes`);
+  const { data } = await axios.get(`api/ranking/likes`);
 
   return data;
 };
 
 export const getMeetingRanking = async (): Promise<MeetingRankingType[]> => {
-  const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/ranking/meeting`);
+  const { data } = await axios.get(`api/ranking/meeting`);
 
   return data;
 };
 
 export const getFoodRanking = async (): Promise<LikesRankingType[]> => {
-  const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/ranking/food`);
+  const { data } = await axios.get(`api/ranking/food`);
 
   return data;
 };
-
