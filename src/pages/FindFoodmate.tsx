7ac7319247578fe216e53341179810ade33be475
DDIDDI <bananaqick@gmail.com>
2023-10-16 18:45:11 +0900

    refactor : 데이터 받아와서 검색 기능 및 필터링 구현

--- src/pages/FindFoodmate.tsx
+++ src/pages/FindFoodmate.tsx
@@ -6,6 +6,18 @@ import { useState, useEffect } from 'react';
 import { MenuLabels } from '../components/findFoodmate/MenuLabels';
 import { PostCardsList } from '../components/common/PostCardsList';
 import { useNavigate } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import {
+  getAllGroups,
+  getCloseGroups,
+  getDateSortedGroups,
+  getSearchGroups,
+  getSelectedMenuGroups,
+} from '../api/groupApi';
+import useCurrentLocation from '../hooks/useCurrentLocation';
+import { DatePickers } from '../components/findFoodmate/DatePickers';
+import { BsSearchHeart } from 'react-icons/bs';
+import { FiRefreshCcw } from 'react-icons/fi';
 
 const categories = [
   { key: 'total', label: '전체' },
@@ -16,8 +28,14 @@ const categories = [
 
 export const FindFoodmate = () => {
   const navigation = useNavigate();
+  const myLocation = useCurrentLocation();
   const [selectedCategory, setSelectedCategory] = useState<string>('total');
   const [menuLabelModalOpened, setMenuLabelModalOpened] = useState<boolean>(false);
+  const [datePickerModalOpened, setDatePickerModalOpened] = useState<boolean>(false);
+  const [selectedDates, setSelectedDates] = useState<string[]>([]);
+  const [selectedMenus, setSelectedMenus] = useState<string[]>([]);
+  const [searchText, setSearchText] = useState('');
+  const [searchedData, setSearchedData] = useState(null);
 
   useEffect(() => {
     if (selectedCategory === 'menu') {
@@ -28,12 +46,79 @@ export const FindFoodmate = () => {
   }, [selectedCategory]);
 
   const handleCategories = (category: string) => {
+    if (selectedCategory !== 'date') {
+      setSelectedDates([]);
+    }
+
+    if (selectedCategory !== 'menu') {
+      setSelectedMenus([]);
+    }
+
+    if (category === 'menu') {
+      setMenuLabelModalOpened((prev) => !prev);
+    } else {
+      setMenuLabelModalOpened(false);
+    }
+
+    if (category === 'date') {
+      setDatePickerModalOpened((prev) => !prev);
+    } else {
+      setDatePickerModalOpened(false);
+    }
+
     setSelectedCategory(category);
   };
 
-  const handleMenuLabelModal = (bool: boolean) => {
-    setMenuLabelModalOpened(bool);
+  const handleMenuLabelModal = (isOpen: boolean) => {
+    setMenuLabelModalOpened(isOpen);
   };
+
+  const handleDatePickerModal = (isOpen: boolean) => {
+    setDatePickerModalOpened(isOpen);
+  };
+
+  const handleSelectedDates = (dates: string[]) => {
+    setSelectedDates(dates);
+  };
+
+  const handleSelectedMenus = (menu: string[]) => {
+    setSelectedMenus(menu);
+  };
+
+  const handleSearchClick = async () => {
+    setSearchText(searchText);
+    if (searchText) {
+      try {
+        const results = await getSearchGroups(searchText);
+        setSearchedData(results.content);
+      } catch (error) {
+        console.error('Error fetching search results:', error);
+      }
+    }
+  };
+
+  const handleRefresh = () => {
+    setSearchedData(null);
+    setSearchText('');
+  };
+
+  let categoryFunction;
+  if (selectedCategory === 'total') {
+    categoryFunction = getAllGroups;
+  } else if (selectedCategory === 'distance') {
+    categoryFunction = myLocation ? () => getCloseGroups(myLocation) : () => null;
+  } else if (selectedCategory === 'date' && selectedDates.length !== 0) {
+    categoryFunction = () => getDateSortedGroups(selectedDates[0], selectedDates[1]);
+  } else if (selectedCategory === 'menu' && selectedMenus.length !== 0) {
+    categoryFunction = () => getSelectedMenuGroups(selectedMenus);
+  }
+
+  const { data, error, isLoading } = useQuery(
+    ['getGroups', selectedCategory, selectedDates, selectedMenus],
+    categoryFunction || getAllGroups,
+  );
+  if (isLoading) return 'Loading...';
+  if (error) return 'error!';
 
   return (
     <BasicPadding>
@@ -41,7 +126,22 @@ export const FindFoodmate = () => {
         <div className="upper">
           <h2>밥 친구 구해요!</h2>
           <div className="input-button-container">
-            <BasicInput $backgdColor="#e8e8e8" placeholder="제목이나 닉네임을 입력하세요" />
+            <div className="input-box">
+              <BasicInput
+                value={searchText}
+                onChange={(e) => setSearchText(e.target.value)}
+                $backgdColor="#e8e8e8"
+                placeholder="제목이나 닉네임을 입력하세요"
+              />
+              <div className="icon">
+                <div role="button" onClick={handleSearchClick}>
+                  <BsSearchHeart />
+                </div>
+                <div role="button" onClick={handleRefresh}>
+                  <FiRefreshCcw />
+                </div>
+              </div>
+            </div>
             <BasicButton $fontSize="12px" onClick={() => navigation('/findfoodmate/newpost')}>
               모임 만들기
             </BasicButton>
@@ -60,12 +160,23 @@ export const FindFoodmate = () => {
           <div className="menu-label">
             {menuLabelModalOpened && (
               <div>
-                <MenuLabels handleMenuLabelModal={handleMenuLabelModal} />
+                <MenuLabels handleMenuLabelModal={handleMenuLabelModal} handleSelectedMenus={handleSelectedMenus} />
+              </div>
+            )}
+          </div>
+          <div className="date-picker">
+            {datePickerModalOpened && (
+              <div>
+                <DatePickers handleDatePickerModal={handleDatePickerModal} handleSelectedDates={handleSelectedDates} />
               </div>
             )}
           </div>
         </div>
-        <PostCardsList />
+        {data.content.length === 0 && !searchedData ? (
+          <div>데이터가 없어요</div>
+        ) : (
+          <PostCardsList groupsData={searchedData || data.content} />
+        )}
       </FindFoodmateContainer>
     </BasicPadding>
   );
@@ -85,6 +196,28 @@ const FindFoodmateContainer = styled.div`
     display: flex;
     gap: 12px;
     margin-top: 24px;
+
+    .input-box {
+      position: relative;
+
+      .icon {
+        position: absolute;
+        right: 16px;
+        top: 6px;
+        display: flex;
+        gap: 12px;
+        font-size: 19px;
+
+        div {
+          cursor: pointer;
+          transition: 0.3s;
+
+          &:hover {
+            color: ${(props) => props.theme.color.ORANGE};
+          }
+        }
+      }
+    }
   }
 
   .filters {
@@ -123,9 +256,14 @@ const FindFoodmateContainer = styled.div`
     }
   }
 
-  .menu-label {
+  .menu-label,
+  .date-picker {
     background-color: #fff;
     position: absolute;
-    bottom: -100px;
+    bottom: -150px;
+  }
+
+  .date-picker {
+    bottom: -55px;
   }
 `;
