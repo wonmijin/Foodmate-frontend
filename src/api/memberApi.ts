8e7f5bd638305d6a3eaa479fc35d4ee7c37adffd
wonmijin <mijin950503@gmail.com>
2023-10-20 23:48:41 +0900

    feat:회원가입 연동
    feat:회원가입 연동

--- src/api/memberApi.ts
+++ src/api/memberApi.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { registerType, emailType, nicknameType } from '../types/registerType';
 
 // 로그인
 export const onSignIn = async (email: string, password: string) => {
@@ -29,6 +30,43 @@ export const kakaoSignIn = async () => {
   try {
     const result = await axios.get('/api/oauth2/authorization/kakao');
     return result.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// 회원가입
+export const registerMember = async ({ email, nickname, password, image, food }: registerType) => {
+  const requestData = {
+    email,
+    nickname,
+    password,
+    image,
+    food,
+  };
+  try {
+    const response = await axios.post('/api/member/signup', requestData);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// 이메일 중복확인
+export const emailConfirm = async ({ email }: emailType) => {
+  try {
+    const response = await axios.get('/api/member/email', { params: { email } });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// 닉네임 중복확인
+export const nicknameConfirm = async ({ nickname }: nicknameType) => {
+  try {
+    const response = await axios.get('/api/member/nickname', { params: { nickname } });
+    return response.data;
   } catch (error) {
     console.error(error);
   }
---
+++ src/types/registerType.ts
@@ -0,0 +1,15 @@
+export interface registerType {
+  email: string;
+  nickname: string;
+  password: string;
+  image: object;
+  food: object;
+}
+
+export interface emailType {
+  email: string;
+}
+
+export interface nicknameType {
+  nickname: string;
+}
