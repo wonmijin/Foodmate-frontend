a12b06aa4865c2848e778d411f5a3c4c6ebd2052
DDIDDI <bananaqick@gmail.com>
2023-10-22 00:07:51 +0900

    feat : 공용 fetchCall 함수 생성

---
+++ src/api/fetchCall.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosError } from 'axios';
+
+const API_BASE_URL = '/api';
+const API_TOKEN = sessionStorage.getItem('accessToken');
+
+const axiosInstance = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    Authorization: `Bearer ${API_TOKEN}`,
+    'Content-Type': 'application/json',
+  },
+});
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const fetchCall = async (
+  method: 'post' | 'put' | 'delete',
+  url: string,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  data?: any,
+) => {
+  try {
+    const response = await axiosInstance[method](url, data);
+    return response.data;
+  } catch (error) {
+    const axiosError = error as AxiosError;
+    console.log(axiosError);
+    throw axiosError;
+  }
+};
