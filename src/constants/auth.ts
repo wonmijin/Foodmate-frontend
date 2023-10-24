86ad3f51b3156684c586c92125ddffe4d2ed2408
Suzy-Lee <mulddang2@naver.com>
2023-10-25 01:11:45 +0900

    feat: auth 상수 추가

---
+++ src/constants/auth.ts
@@ -0,0 +1,14 @@
+/**
+ * 로그인 사용자 닉네임
+ */
+export const NICKNAME = 'nickname';
+
+/**
+ * Access Token
+ */
+export const ACCESS_TOKEN = 'accessToken';
+
+/**
+ * Refresh Token
+ */
+export const REFRESH_TOKEN = 'refreshToken';
