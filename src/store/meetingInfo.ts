9074ff1707090ce5033c4c79e996e84a74ef742f
DDIDDI <bananaqick@gmail.com>
2023-10-24 12:34:17 +0900

    feat : 신청 현황 카테고리 atom 추가

--- src/store/meetingInfo.ts
+++ src/store/meetingInfo.ts
@@ -4,3 +4,8 @@ export const requestCategory = atom<string>({
   key: 'requestCategoryAtom',
   default: '받은 요청',
 });
+
+export const subscriptionCategory = atom<string>({
+  key: 'subscriptionCategoryAtom',
+  default: '신청 현황',
+});
