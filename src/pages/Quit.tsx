11b09d4c4ac9ed434ced54586440cfd67a60f533
DDIDDI <bananaqick@gmail.com>
2023-10-25 15:13:44 +0900

    feat : 회원 탈퇴 구현

--- src/pages/Quit.tsx
+++ src/pages/Quit.tsx
@@ -1,10 +1,15 @@
+import { useState, useEffect } from 'react';
 import { BasicPadding } from '../components/common/BasicPadding';
 import { SideMenu } from '../components/common/SideMenu';
 import { MYPAGE_CATEGORY } from '../constants/mypage';
 import styled from 'styled-components';
-import { useForm, SubmitHandler } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { LoginButton } from '../components/common/LoginButton';
 import { IoCaretForwardOutline } from 'react-icons/io5';
+import { fetchCall } from '../api/fetchCall';
+import { AxiosError } from 'axios';
+import { deleteCookie } from '../utils/deleteCookie';
+import { useNavigate } from 'react-router-dom';
 
 interface QuitForm {
   password: string;
@@ -12,14 +17,38 @@ interface QuitForm {
 }
 
 export const Quit = () => {
+  const [myEmail, setMyEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigation = useNavigate();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const myInfoData = await fetchCall('get', '/member');
+        setMyEmail(myInfoData.email);
+      } catch (error) {
+        console.error('myInfoData', error);
+      }
+    })();
+  }, []);
+
   const {
     register,
     formState: { errors },
-    handleSubmit,
   } = useForm<QuitForm>({ mode: 'onBlur' });
 
-  const onSubmitHandler: SubmitHandler<QuitForm> = (data) => {
-    console.log(data);
+  const handleSubmit = async () => {
+    try {
+      await fetchCall('delete', '/member/delete', {
+        password,
+      });
+      sessionStorage.clear();
+      deleteCookie('refreshToken');
+      navigation('/');
+    } catch (error) {
+      const axiosError = error as AxiosError;
+      alert(axiosError.response?.data);
+    }
   };
 
   return (
@@ -39,39 +68,41 @@ export const Quit = () => {
                 입력하신 개인정보 모두 삭제 처리됩니다.
               </p>
             </QuitText>
-            <form onSubmit={handleSubmit(onSubmitHandler)}>
-              <hr></hr>
-              <QuitPaddig>
-                <QuitInBox>
-                  <p>아이디</p>
-                  <div></div>
-                </QuitInBox>
+            <hr></hr>
+            <QuitPaddig>
+              <QuitInBox>
+                <p>아이디</p>
+                <div>{myEmail}</div>
+              </QuitInBox>
 
-                <QuitInBox>
-                  <p>비밀번호</p>
-                  <div>
-                    <input
-                      type="password"
-                      {...register('password', {
-                        required: '필수 값입니다.',
-                        minLength: {
-                          value: 8,
-                          message: '비밀번호는 숫자, 영문 대문자, 소문자, 특수문자를 포함한 8글자 이상이어야 합니다.',
-                        },
-                        pattern: {
-                          value: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
-                          message: '비밀번호는 숫자, 영문 대문자, 소문자, 특수문자를 포함한 8글자 이상이어야 합니다.',
-                        },
-                      })}
-                      placeholder="비밀번호를 입력해주세요."
-                    />
-                    <p>{errors?.password?.message}</p>
-                  </div>
-                </QuitInBox>
-              </QuitPaddig>
-              <hr></hr>
-              <LoginButton $fontSize="16px">회원 탈퇴</LoginButton>
-            </form>
+              <QuitInBox>
+                <p>비밀번호</p>
+                <div>
+                  <input
+                    type="password"
+                    {...register('password', {
+                      required: '필수 값입니다.',
+                      minLength: {
+                        value: 8,
+                        message: '비밀번호는 숫자, 영문 대문자, 소문자, 특수문자를 포함한 8글자 이상이어야 합니다.',
+                      },
+                      pattern: {
+                        value: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+                        message: '비밀번호는 숫자, 영문 대문자, 소문자, 특수문자를 포함한 8글자 이상이어야 합니다.',
+                      },
+                    })}
+                    placeholder="비밀번호를 입력해주세요."
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <p>{errors?.password?.message}</p>
+                </div>
+              </QuitInBox>
+            </QuitPaddig>
+            <hr></hr>
+            <LoginButton $fontSize="16px" onClick={handleSubmit}>
+              회원 탈퇴
+            </LoginButton>
           </QuitBox>
         </QuitWrap>
       </BasicPadding>
@@ -109,7 +140,7 @@ const QuitBox = styled.div`
 
 const QuitInBox = styled.div`
   display: flex;
-  align-items: flex-start;
+  align-items: center;
   padding: 10px 0;
 
   p {
